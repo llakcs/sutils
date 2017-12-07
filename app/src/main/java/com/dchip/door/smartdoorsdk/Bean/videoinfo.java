aeb263602b0e67b49664a8b54953f8a490e6d1d6
llakcs <llacs@163.com>
2017-12-07 16:26:17 +0800

    commit

---
+++ app/src/main/java/com/dchip/door/smartdoorsdk/Bean/videoinfo.java
@@ -0,0 +1,37 @@
+package com.dchip.door.smartdoorsdk.Bean;
+
+/**
+ * Created by llakcs on 2017/11/28.
+ */
+
+public class videoinfo {
+    public String lockID;
+
+    public String Uid;
+
+    public String addr;
+
+    public String getLockID() {
+        return lockID;
+    }
+
+    public void setLockID(String lockID) {
+        this.lockID = lockID;
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
---
+++ app/src/main/java/com/dchip/door/smartdoorsdk/video/VideoManager.java
@@ -0,0 +1,16 @@
+package com.dchip.door.smartdoorsdk.video;
+
+import android.content.Context;
+
+import com.hyphenate.chat.EMCallStateChangeListener;
+import com.hyphenate.media.EMCallSurfaceView;
+
+/**
+ * Created by llakcs on 2017/11/30.
+ */
+
+public interface VideoManager {
+
+    void cmLogin(String user,String pass);
+
+}
