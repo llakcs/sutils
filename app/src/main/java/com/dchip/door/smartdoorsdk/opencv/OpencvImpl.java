f4ef4c3aae7ac45a7a46369f362abe91dfe06019
llakcs <llacs@163.com>
2017-12-18 15:21:59 +0800

    fix bug

--- app/src/main/java/com/dchip/door/smartdoorsdk/opencv/OpencvImpl.java
+++ app/src/main/java/com/dchip/door/smartdoorsdk/opencv/OpencvImpl.java
@@ -65,14 +65,18 @@ public class OpencvImpl implements OpencvManager,CameraBridgeViewBase.CvCameraVi
     private DetectionListner mDetection;
     @Override
     public void onResume() {
-        if (!OpenCVLoader.initDebug()) {
-            LogUtil.e(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, mContext, mLoaderCallback);
-        } else {
-            LogUtil.e(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (!OpenCVLoader.initDebug()) {
+                    LogUtil.e(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+                    OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, mContext, mLoaderCallback);
+                } else {
+                    LogUtil.e(TAG, "OpenCV library found inside package. Using it!");
+                    mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+                }
+            }
+        }).start();
     }
 
     @Override
