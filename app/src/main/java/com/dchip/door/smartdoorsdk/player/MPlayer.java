448057852a509c6f7a1e4b8d3b73b770a35dcfd3
dchip_hzh <494198050@qq.com>
2017-12-27 12:23:11 +0800

    not play when create

--- app/src/main/java/com/dchip/door/smartdoorsdk/player/MPlayer.java
+++ app/src/main/java/com/dchip/door/smartdoorsdk/player/MPlayer.java
@@ -357,7 +357,7 @@ public class MPlayer implements IMPlayer,MediaPlayer.OnBufferingUpdateListener,
             }
             log("surface被创建");
             playNext(mIndex);
-            playStart();
+//            playStart();
         }
     }
 
