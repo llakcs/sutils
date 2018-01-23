6b05c6e9ffd8a7894947eb19f559d8258c6f919f
dchip_hzh <494198050@qq.com>
2018-01-23 10:57:26 +0800

    fix return

--- app/src/main/jni/devicecontrol/devicecontrol.cpp
+++ app/src/main/jni/devicecontrol/devicecontrol.cpp
@@ -20,6 +20,8 @@
 #define SLOT_SAM2    2
 #define SLOT_SAM3    3
 
+#define TAG   "deviceControlJni"
+
 #define PCD_CMD(n, m) ((n << 4) | m)
 
 
@@ -42,12 +44,12 @@ IFD_PARAM UsrParam;
 unsigned char StrToHex(char *src, int len) {
     int i = 0;
     unsigned char a = 0;
-    //__android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "start str to hex ...");
+    //__android_log_print(ANDROID_LOG_VERBOSE, TAG, "start str to hex ...");
     if (src == NULL) return 0;
     else {
         while (len--) {
             //        PrtMsg("src + %d = 0x%X\n", i, *(src+i));
-            // __android_log_print(ANDROID_LOG_VERBOSE, "Pn512","src + %d = %c\n", i, *(src+i));
+            // __android_log_print(ANDROID_LOG_VERBOSE, TAG,"src + %d = %c\n", i, *(src+i));
             usleep(1000);
             if (*(src + i) >= '0' && *(src + i) <= '9')
                 a = a * 16 + (src[i] - '0');
@@ -59,7 +61,7 @@ unsigned char StrToHex(char *src, int len) {
 
             ++i;
         }
-        // __android_log_print(ANDROID_LOG_VERBOSE, "Pn512","a=0x%x", a);
+        // __android_log_print(ANDROID_LOG_VERBOSE, TAG,"a=0x%x", a);
         return a;
     }
 }
@@ -92,14 +94,14 @@ int CardPowerOn() {
     } while (retry < 3);
 
     if (retry >= 3) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "Operation fail with Errorcode = %lX",
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "Operation fail with Errorcode = %lX",
                             retval);
         return -1;
     } else {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "ATR Len: %d",
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "ATR Len: %d",
                             UsrParam.oDataLen);
         //  for (i = 0; i < UsrParam.oDataLen; i++) {
-        //     __android_log_print(ANDROID_LOG_VERBOSE, "Pn512","RecBuf[%d]= 0x%02X",i, RecBuf[i]);
+        //     __android_log_print(ANDROID_LOG_VERBOSE, TAG,"RecBuf[%d]= 0x%02X",i, RecBuf[i]);
         // }
         return 0;
     }
@@ -113,11 +115,11 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Card_Pn512_1OpenAn
     long retval;
     fd = open("/dev/pcd", O_RDWR);
     if (fd < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/pcd Pn512 device Error..");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/pcd Pn512 device Error..");
         return 0;
     } else {
         //CardPowerOn();
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/pcd Pn512 device sucess");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/pcd Pn512 device sucess");
         return 1;
     }
 }
@@ -129,7 +131,7 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Card_Pn512_1PowerO
     // TODO
     long retval;
     if (retval = ioctl(fd, 0x20, &UsrParam) < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "close /dev/pcd Pn512 device Error..");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "close /dev/pcd Pn512 device Error..");
         return 0;
     } else {
         if (fd > 0) {
@@ -148,16 +150,16 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Card_CardChecked(J
     unsigned char chk[6] = {0xFF,0x88,0x00,0x07,0x60,0x00};
     int i = 0;
     CardPowerOn();
-    __android_log_print(ANDROID_LOG_WARN, "Pn512", "CardChecked");
+    __android_log_print(ANDROID_LOG_WARN, TAG, "CardChecked");
     UsrParam.p_oBuf = RecBuf;
     UsrParam.p_iBuf = chkPwd;
     UsrParam.iDataLen = 11;
     UsrParam.oDataLen = 271;
     int retval = 0;
     if ((retval = ioctl(fd, 0x30, &UsrParam) ) >= 0) {
-        __android_log_print(ANDROID_LOG_WARN, "Pn512", "chkPwd ioctl >= 0");
+        __android_log_print(ANDROID_LOG_WARN, TAG, "chkPwd ioctl >= 0");
        if( RecBuf[0] == 0x90 && RecBuf[1] == 0x0 ){
-        __android_log_print(ANDROID_LOG_WARN, "Pn512", "RecBuf RecBuf = 9000");
+        __android_log_print(ANDROID_LOG_WARN, TAG, "RecBuf RecBuf = 9000");
             memset(RecBuf,0,sizeof(RecBuf));
             CardPowerOn();
             UsrParam.p_oBuf = RecBuf;
@@ -166,7 +168,7 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Card_CardChecked(J
             UsrParam.oDataLen = 271;
             int retval = 0;
             if ((retval = ioctl(fd, 0x30, &UsrParam)) >= 0) {
-             __android_log_print(ANDROID_LOG_WARN, "Pn512", "chk ioctl > 0");
+             __android_log_print(ANDROID_LOG_WARN, TAG, "chk ioctl > 0");
                 if(RecBuf[0] == 0x90 && RecBuf[1] == 0x0){
                     return  true;
                 }
@@ -185,7 +187,7 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Card_CardDetect(JN
         int ret = 0;
         ret = read(fd, &card_val, 1);
         if (ret < 0) {
-            __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "Read Error!\n");
+            __android_log_print(ANDROID_LOG_VERBOSE, TAG, "Read Error!\n");
             return false;
         }
         return true;
@@ -200,29 +202,29 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Card_Pn512_1CardCm
 
     char *toJava;
     char *cmd = (char *) env->GetStringUTFChars(cmdstr, 0);
-    //__android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "cmd=%s",cmd);
+    //__android_log_print(ANDROID_LOG_VERBOSE, TAG, "cmd=%s",cmd);
     UsrParam.p_oBuf = RecBuf;
     UsrParam.p_iBuf = CmdBuf;
     // TODO
     CardPowerOn();
-    // __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "power on pn512 sucess and start operation");
+    // __android_log_print(ANDROID_LOG_VERBOSE, TAG, "power on pn512 sucess and start operation");
     char *pBuf;
     pBuf = (char *) cmd;
     len = strlen(pBuf);
-    // __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", " start operation pBuf");
-    // __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", " pBuf len=%d,pBuf=%s",len,pBuf);
+    // __android_log_print(ANDROID_LOG_VERBOSE, TAG, " start operation pBuf");
+    // __android_log_print(ANDROID_LOG_VERBOSE, TAG, " pBuf len=%d,pBuf=%s",len,pBuf);
 
     while (*(pBuf + j) != '\0' && j <= len) {
         CmdBuf[i] = StrToHex(pBuf + j, 2);
         j += 2;
         i++;
     }
-    __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "i = %d\t pBuf = %s\n", i, pBuf);
+    __android_log_print(ANDROID_LOG_VERBOSE, TAG, "i = %d\t pBuf = %s\n", i, pBuf);
     UsrParam.p_iBuf = CmdBuf;
     UsrParam.iDataLen = i;
     UsrParam.oDataLen = 271;
     if ((retval = ioctl(fd, 0x30, &UsrParam)) < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "Operation fail with errorcode = %08X",
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "Operation fail with errorcode = %08X",
                             retval);
         return env->NewStringUTF("write pn512 fail");
     } else {
@@ -234,11 +236,11 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Card_Pn512_1CardCm
         }
         for (i = 0; i < UsrParam.oDataLen * 2; i++) {
             dst[i] = HexToChar(str[i]);
-            __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", " dst[%d] = %c\n", i, dst[i]);
+            __android_log_print(ANDROID_LOG_VERBOSE, TAG, " dst[%d] = %c\n", i, dst[i]);
         }
         dst[UsrParam.oDataLen * 2] = '\0';
         toJava = dst;
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", " toJava = %s\n", toJava);
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, " toJava = %s\n", toJava);
         env->ReleaseStringUTFChars(cmdstr, cmd);
         return env->NewStringUTF(toJava);
     }
@@ -258,10 +260,10 @@ JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Lock_open
         (JNIEnv *env, jobject obj) {
     fd_l = open("/dev/door_gpio", O_RDWR);
     if (fd_l < 0) {
-    __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/door_gpio Error..");
+    __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/door_gpio Error..");
         return false;
 } else {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/door_gpio  fd = %d", fd_l);
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/door_gpio  fd = %d", fd_l);
         return true;
     }
 }
@@ -276,7 +278,7 @@ Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Lock_close
 (JNIEnv * env , jobject obj ) {
 if ( fd_l > 0 ) {
 close(fd_l);
-__android_log_print(ANDROID_LOG_VERBOSE, "Pn512" , "led_gpio device close" ) ;
+__android_log_print(ANDROID_LOG_VERBOSE, TAG , "led_gpio device close" ) ;
 }
 }
 
@@ -291,15 +293,15 @@ __android_log_print(ANDROID_LOG_VERBOSE, "Pn512" , "led_gpio device close" ) ;
 JNIEXPORT jint
 JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Pn512Lock_ioctl
         (JNIEnv * env, jobject obj,jint cmd, jint arg){
-    //__android_log_print(ANDROID_LOG_VERBOSE,"Pn512","in ioctl fd_l=%d cmd=%d arg=%d",fd_l,cmd,arg);
+    //__android_log_print(ANDROID_LOG_VERBOSE,TAG,"in ioctl fd_l=%d cmd=%d arg=%d",fd_l,cmd,arg);
     int ret;
 
     ret = ioctl(fd_l, cmd, arg);
     if(ret<0)
-        __android_log_print(ANDROID_LOG_VERBOSE,"Pn512","ioctl the %d gpio fail",arg);
+        __android_log_print(ANDROID_LOG_VERBOSE,TAG,"ioctl the %d gpio fail",arg);
     else{
-//        if (cmd==3) __android_log_print(ANDROID_LOG_VERBOSE,"Pn512","ioctl the %d gpio get %d",arg,ret);
-//        else __android_log_print(ANDROID_LOG_VERBOSE,"Pn512","ioctl the %d gpio to %d",arg,cmd);
+//        if (cmd==3) __android_log_print(ANDROID_LOG_VERBOSE,TAG,"ioctl the %d gpio get %d",arg,ret);
+//        else __android_log_print(ANDROID_LOG_VERBOSE,TAG,"ioctl the %d gpio to %d",arg,cmd);
     }
     if (ret>0) return 0;
     else return 1;
@@ -313,10 +315,10 @@ JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_HumanCheck_open
         (JNIEnv * env, jobject obj){
     fd_hc = open("/dev/human_det", O_RDWR);
     if (fd_hc < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/human_det Error..");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/human_det Error..");
         return false;
     } else {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/human_det  fd = %d", fd_hc);
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/human_det  fd = %d", fd_hc);
         return true;
     }
 }
@@ -325,7 +327,7 @@ JNIEXPORT jint
 JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_HumanCheck_check
         (JNIEnv * env, jobject obj){
     if (fd_hc < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/human_det device not open Error.. ");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/human_det device not open Error.. ");
         return -99;
     }
     int ret = ioctl(fd_hc, 3, 0);
@@ -346,10 +348,10 @@ JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Led_open
         (JNIEnv * env, jobject obj){
     fd_led = open("/dev/sys_stat", O_RDWR);
     if (fd_led < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/sys_stat Error..");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/sys_stat Error..");
         return false;
     } else {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/sys_stat  fd = %d", fd_hc);
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/sys_stat  fd = %d", fd_hc);
         return true;
     }
 }
@@ -358,7 +360,7 @@ JNIEXPORT jint
 JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Led_ioCtl
         (JNIEnv * env, jobject obj,jint cmd, jint arg){
     if (fd_led < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/sys_stat device not open Error.. ");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/sys_stat device not open Error.. ");
         return -99;
     }
     int ret = ioctl(fd_led, cmd, arg);
@@ -378,7 +380,7 @@ JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_LockSwitch_chec
         (JNIEnv * env, jobject obj){
         int fd_switch = open("/dev/push_switch", O_RDWR);
     if (fd_switch < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/push_switch device not open Error.. ");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/push_switch device not open Error.. ");
         return -99;
     }
     int ret = ioctl(fd_switch, 3, 0);
@@ -392,7 +394,7 @@ JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_LockBreak_check
         (JNIEnv * env, jobject obj){
         int fd_push = open("/dev/safe_sw", O_RDWR);
     if (fd_push < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/push_switch device not open Error.. ");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/push_switch device not open Error.. ");
         return -99;
     }
     int ret = ioctl(fd_push, 3, 0);
@@ -408,10 +410,10 @@ JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Steer_open
         (JNIEnv * env, jobject obj){
     fd_s = open("/dev/steer_out", O_RDWR);
     if (fd_s < 0) {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/steer_out Error..");
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/steer_out Error..");
         return false;
     } else {
-        __android_log_print(ANDROID_LOG_VERBOSE, "Pn512", "open /dev/steer_out  fd = %d", fd_s);
+        __android_log_print(ANDROID_LOG_VERBOSE, TAG, "open /dev/steer_out  fd = %d", fd_s);
         return true;
     }
 }
@@ -419,18 +421,18 @@ JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Steer_open
 JNIEXPORT jint
 JNICALL Java_com_dchip_door_smartdoorsdk_deviceControl_nativeLev_Steer_ioctl
         (JNIEnv * env, jobject obj,jint cmd,jint arg){
-    __android_log_print(ANDROID_LOG_VERBOSE,"Pn512","in ioctl fd_s=%d cmd=%d arg=%d",fd_l,cmd,arg);
+    __android_log_print(ANDROID_LOG_VERBOSE,TAG,"in ioctl fd_s=%d cmd=%d arg=%d",fd_l,cmd,arg);
     int ret;
 
     ret = ioctl(fd_s, cmd, arg);
     if(ret<0)
-        __android_log_print(ANDROID_LOG_VERBOSE,"Pn512","ioctl the gpio fail");
+        __android_log_print(ANDROID_LOG_VERBOSE,TAG,"ioctl the gpio fail");
     else{
-//        if (cmd==3) __android_log_print(ANDROID_LOG_VERBOSE,"Pn512","ioctl the %d gpio get %d",arg,ret);
-//        else __android_log_print(ANDROID_LOG_VERBOSE,"Pn512","ioctl the %d gpio to %d",arg,cmd);
+        if (cmd==3) __android_log_print(ANDROID_LOG_VERBOSE,TAG,"ioctl the %d gpio get %d",arg,ret);
+//        else __android_log_print(ANDROID_LOG_VERBOSE,TAG,"ioctl the %d gpio to %d",arg,cmd);
     }
-    if (ret>0) return 0;
-    else return 1;
+    return ret;
+
 }
 
 JNIEXPORT void
